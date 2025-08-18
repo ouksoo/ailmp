5da9e2d3fc4ea879466572b3c28855490fa66d45
ouksoo <ouksoo@nate.com>
2025-08-18 20:07:47 +0900

    레이아웃 및 컴포넌트 진행 중

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,65 @@
+var AILMP = {
+	stopDocumentClick : 'div.header-right, .search-box',
+	gnbMenuActive : function(){ 
+		//메뉴 활성화 시 서브메뉴 아코디언
+		$(".depth-1").click(function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			let submenu = $(this).next(".submenus");
+			let parentLink = $(this);
+
+			if (!submenu.is(":visible")) {
+				$(".submenus").slideUp();
+				$(".depth-1").removeClass("active");
+
+				submenu.stop(true, true).slideDown();
+				parentLink.addClass("active");
+			} else {
+				submenu.stop(true, true).slideUp();
+				parentLink.removeClass("active");
+			}
+		});
+
+		//우측 메뉴 활성
+		$('div.header-right a.tops.d').on('click', function() {
+			$('aside.lnb-wrapper').addClass('active');
+			$('.film-normal').fadeIn();
+
+			$('a.menu-close').click(function() {
+				$('aside.lnb-wrapper').removeClass('active');
+				$('.film-normal').fadeOut();
+			});
+		});
+
+		//상단 검색 영역 활성
+		$('div.header-right a.tops.a').on('click', function(e) {
+			e.stopPropagation();
+			$('div.header-right .search-box').fadeIn();
+		});
+
+		//상단 로그인, 회원가입 활성
+		$('div.header-right div.b').on('mouseenter mouseleave', function(e) {
+			const $target = $('div.header-right div.b p.bloom-login');
+
+			if (e.type === 'mouseenter') {
+				$target.css('display', 'block');
+			} else if (e.type === 'mouseleave') {
+				$target.css('display', 'none');
+			}
+		});
+
+		//prevent document click 
+		$(AILMP.stopDocumentClick).on('click', function(e) {
+			e.stopPropagation();
+		});
+	}
+}
+
 $(document).ready(function() {
+	AILMP.gnbMenuActive();
 
+	$(document).click(function () {
+		$('div.header-right .search-box').fadeOut();
+	});
 });
-
-
